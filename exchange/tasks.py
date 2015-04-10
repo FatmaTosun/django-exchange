fbcc809a48b89bf97f05ffce73070ed08849d004
FatmaTosun <info@fatmatosun.net>
2015-04-10 13:41:06 +0300

    bacokoff package is added to retry ConnectionError

--- exchange/tasks.py
+++ exchange/tasks.py
@@ -1,8 +1,10 @@
 import celery
-
+import backoff
+from requests.exceptions import ConnectionError
 from exchange.conversion import update_rates
 
 
 @celery.task
+@backoff.on_exception(backoff.expo, ConnectionError, max_tries=3)
 def update_task(adapter_class_name=None):
     update_rates(adapter_class_name=adapter_class_name)
